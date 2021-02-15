0795f7e7d672e19f1a563e74cd85c010afae9bea
MovementUI <vilmos.thenics@gmail.com>
2021-02-15 12:42:40 +0100

    changing fonts

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 12;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"anonymous-pro:size=9"};
-static const char dmenufont[]       = "anonymous-pro:size=9";
+static const char *fonts[]          = {"monospace:size=9"};
+static const char dmenufont[]       = "monospace:size=9";
 static const char col_black[]	    = "#000000";
 static const char col_white[]	    = "#ffffff";
 static const char col_gray1[]       = "#222222";
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_black, "-sf", col_custom, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
